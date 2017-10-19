a607d6a6bf260dbcb1807773f8d7b42af80dee14
Arnom Maciel <arnomrj@gmail.com>
2017-10-19 00:16:42 -0200

    Primeira tela OK

--- App.js
+++ App.js
@@ -1,57 +1,68 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
 import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Image
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const logo = require('./assets/imgs/logo.png');
+const btnJogar = require('./assets/imgs/botao_jogar.png');
+const sobre = require('./assets/imgs/sobre_jogo.png');
+const outros = require('./assets/imgs/outros_jogos.png');
 
-export default class App extends Component<{}> {
+
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+      <View style={styles.content.view}>
+        <View style={styles.content.jogo.view}>
+          <Image source={logo} style={styles.content.jogo.moeda}/>
+          <Image source={btnJogar} style={styles.content.jogo.botao}/>
+        </View>
+        <View style={styles.content.extras.view}>
+          <Image source={sobre} style={styles.content.extras.sobre}/>
+          <Image source={outros} style={styles.content.extras.outros}/>
+        </View>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+const styles = {
+  content:{
+    view:{
+      marginTop:20,
+      flex:1,
+      backgroundColor: '#61bd8c'
+    },
+    jogo:{
+      view:{
+        flex:10,
+        justifyContent: 'center',
+        alignItems: 'center'
+      },
+      moeda:{
+        
+      },
+      botao:{
+        marginTop: 10
+      }
+    },
+    extras:{
+      view:{
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+      },
+      outros:{
+
+      },
+      extras:{
+
+      }
+    }
+  }
+}
